85272450ac9ac645e7fa195d0267976b0660712a
shunichiYasuda <34476078+shunichiYasuda@users.noreply.github.com>
2022-02-28 14:38:33 +0900

    Basic function complete

--- GrahpViewer/src/main/java/com/GenSci/tools/GrahpViewer/PrimaryController.java
+++ GrahpViewer/src/main/java/com/GenSci/tools/GrahpViewer/PrimaryController.java
@@ -2,11 +2,27 @@ package com.GenSci.tools.GrahpViewer;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    Button execBtn;
+    @FXML
+    TextArea log;
+    @FXML
+    Button quitBtn;
+    @FXML
+    Canvas canvas;
+    
+    //
+    public void quitAction() {
+    	System.exit(0);
+    }
+    //
+    public void execAction() {
+    	log.appendText("Hello World\n");
     }
 }
--- GrahpViewer/src/main/java/module-info.java
+++ GrahpViewer/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.GenSci.tools.GrahpViewer {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires transitive javafx.graphics;
     opens com.GenSci.tools.GrahpViewer to javafx.fxml;
     exports com.GenSci.tools.GrahpViewer;
 }
